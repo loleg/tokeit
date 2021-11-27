4c6cd046d02f422fbd4f04619d1d79053994ade8
Oleg Lavrovsky <oleg@datalets.ch>
2021-11-27 21:56:47 +0100

    Every 🧍Person counts

--- viz/src/layers.js
+++ viz/src/layers.js
@@ -1,7 +1,7 @@
 import { GeoJsonLayer, ColumnLayer } from "@deck.gl/layers";
 
-const DATA_POPULATION = require("../data/centroids.json")
-const DATA_REFERENCE = require("../data/hexgrid.geojson")
+const DATA_POPULATION = require("../data/centroids.json");
+const DATA_REFERENCE = require("../data/hexgrid.geojson");
 
 // https://deck.gl/docs/api-reference/layers/column-layer
 export const populationLayer = (mapState) =>
@@ -15,7 +15,7 @@ export const populationLayer = (mapState) =>
     pickable: false,
     elevationScale: 0.8,
     getPosition: (d) => [d.centroid[1], d.centroid[0]],
-    getFillColor: (d) => [0, 0, (d.value-100)*20, 255],
+    getFillColor: (d) => [10, 10, (d.value-100)*20, 255],
     getLineColor: [0, 0, 0],
     getElevation: (d) => d.value
   });
@@ -28,7 +28,7 @@ export const referenceLayer = (mapState) =>
     // Styles
     filled: true,
     stroked: false,
-    getFillColor: [0, 0, 0, 80],
+    getFillColor: [255, 255, 255, 80],
     // Interactive props
     pickable: true,
     autoHighlight: true,
--- viz/src/map.js
+++ viz/src/map.js
@@ -11,9 +11,9 @@ const INITIAL_VIEW_STATE = {
 };
 
 const MAP_STYLE =
-  require('../data/swiss_style.json');
+  // require('../data/swiss_style.json');
+  'https://api.maptiler.com/maps/ch-swisstopo-lbm-dark/style.json?key=7r2P9F2eOXwOW6g3HfCo';
   // 'https://basemaps.cartocdn.com/gl/dark-matter-nolabels-gl-style/style.json';
-  // 'https://api.maptiler.com/tiles/v3/tiles.json?key=7r2P9F2eOXwOW6g3HfCo';
   // 'https://raw.githubusercontent.com/nst-guide/osm-liberty-topo/gh-pages/style.json';
 
 class MapState {
@@ -45,6 +45,8 @@ export function initMap(containerElement) {
     hash: true
   });
 
+  const POP_TOTAL = 83215;
+
   function calcTokits(num) {
     return Math.round(num / 150);
   }
@@ -54,6 +56,12 @@ export function initMap(containerElement) {
   function calcTokits2plus(num) {
     let c = calcTokits(num);
     return Math.round(c * 1.2) - c;
+  }
+  function calcTokitsPercent(num) {
+    let pct = 100*num / POP_TOTAL;
+    if (pct == 0) return "0";
+    if (pct < 1) return "< 1";
+    return Math.round(pct);
   }
 
   const deck = new Deck({
@@ -72,19 +80,20 @@ export function initMap(containerElement) {
     },
     getTooltip: ({object}) => object && object.properties && {
       html:
-            `<div class="coord">&#x1F4CC; ${object.properties.label}</div>` +
-            `<div class="col">` +
-              `<div class="num">${calcTokits(object.properties.NUMPOINTS)}</div>` +
-              `<span class="label">in 2020</span>` +
-              `<div class="tower" style="height:${calcTokits(object.properties.NUMPOINTS)}em"></div>` +
-            // `</div><div class="col future">` +
-            //   `<div class="num">${calcTokits2(object.properties.NUMPOINTS)}</div>` +
+        `<div class="coord">&#x1F4CC; ${object.properties.label}</div>` +
+        `<div class="col">` +
+          (calcTokits(object.properties.NUMPOINTS) > 0 ?
+            `<div class="num">${calcTokits(object.properties.NUMPOINTS)}</div>` +
+            `<span class="label">in 2020</span>` +
+            `<div class="tower" style="height:${calcTokits(object.properties.NUMPOINTS)}em"></div>` +
+            (calcTokits2plus(object.properties.NUMPOINTS) > 0 ?
               `<div class="future">` +
+                `<div class="tower" style="height:${calcTokits2plus(object.properties.NUMPOINTS)}em"></div>` +
                 `<div class="num">+${calcTokits2plus(object.properties.NUMPOINTS)}</div>` +
                 `<span class="label">in 2030</span>` +
-                `<div class="tower" style="height:${calcTokits2plus(object.properties.NUMPOINTS)}em"></div>` +
-              `</div>` +
-            `</div>`
+              `</div>` : ``) : ``) +
+          `<span class="label actual">🧍 ${object.properties.NUMPOINTS}</span>` +
+        `</div>`
     }
   });
 
